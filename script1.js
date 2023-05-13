5616d1c4313ef63d47c1bb419e3fad3f292d5e46
Boris Romanenko <romanenkobo87@gmail.com>
2023-05-13 16:27:36 +0200

    JS first home work

---
+++ script1.js
@@ -0,0 +1,65 @@
+// let radius = prompt("Please enter a radius");
+// let result = 3.14 * radius ** 2;
+// alert(result);
+
+// let t_c = prompt ("Please enter a temperature");
+// let t_f = t_c *1.8 + 32;
+// alert (t_f);
+
+// let days = 555 / 24;
+// let hours = 555 % 24;
+// alert(days);
+// alert (hours);
+
+// let order_cost = 1000;
+// let discount = 0.15;
+// let summe =order_cost - order_cost * discount;
+// alert (summe);
+
+// let order_cost = 1000;
+// let order_paid = 750;
+// let discount = (order_cost - order_paid);
+// let percent = (discount * 100)/order_cost;
+// alert (percent);
+
+// let firstnum = +prompt ("Please enter a first number");
+// let secondnum = +prompt ("Please enter a second number");
+// let thirdnum = +prompt ("Please enter a third number");
+// let result = (firstnum + secondnum + thirdnum)/3;
+// alert (result);
+
+// let age = 25;
+// if (age < 18) {
+//    alert ('Enter is vorbid')
+// } else {
+//    alert ('You are wellcome')
+// }
+
+// let tmp = 20;
+// if (temp > 20) {
+//    alert ('Hot')
+// } else if (tmp < 20){
+//    alert (Cold)
+// } else {
+//    alert ('comfort')
+// }
+
+// let firstnum = +prompt ('Please enter first number');
+// let secondnum = +prompt ('Please enter second number');
+// if (firstnum > secondnum){
+//    alert('First number is bigger')
+// } else if (firstnum < secondnum){
+//    alert('Second number is bigger')
+// } else {
+//     alert('The numbers are even' )
+// }
+
+// let firstnum = +prompt ('Please enter your count condition');
+// let secondnum = +prompt ('Please enter order amount');
+// if (firstnum >= secondnum){
+//    alert('Your order is paid')
+// } else {
+//    let result = secondnum - firstnum;
+//    alert(`There are not enough money on your count. You must enter ${result} dollars more`);
+// }
+
