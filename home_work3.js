ccad7ed1ce93c1db28bea09f380bc6e70c75cc1d
Boris Romanenko <romanenkobo87@gmail.com>
2023-05-22 19:04:44 +0200

    new

--- home_work3.js
+++ home_work3.js
@@ -4,33 +4,33 @@
 // Задание 2. Напишите функцию, которая принимает товары (массив объектов) и возвращает строку 'Самый дорогой товар <НАЗВАНИЕ> стоит  EUR'.
 
 let products = [
-    {title: 'Iphone', price: 800, quantity: 50},
-    {title: 'Samsung', price: 500, quantity: 39},
-    {title: 'Nokia', price: 200, quantity: 19}
-]
+  { title: "Iphone", price: 800, quantity: 50 },
+  { title: "Samsung", price: 500, quantity: 39 },
+  { title: "Nokia", price: 200, quantity: 19 },
+];
 
 function most_expensive(products) {
-    let expensive = products[0];
-    for (let i =1; i < products.length; i++){
-        if (expensive.price < products[i].price){
-            expensive = products[i];
-        }
+  let expensive = products[0];
+  for (let i = 1; i < products.length; i++) {
+    if (expensive.price < products[i].price) {
+      expensive = products[i];
     }
-    alert(`Самый дорогой товар ${expensiv.title} стоит ${expensiv.price} EUR`);
+  }
+  alert(`Самый дорогой товар ${expensiv.title} стоит ${expensiv.price} EUR`);
 }
 
 // Задание 3. Напишите функцию, которая принимает товары (массив объектов) и возвращает их общую сумму (скаляр).
 
 let products2 = [
-    {title: 'Iphone', price: 800, quantity: 50},
-    {title: 'Samsung', price: 500, quantity: 39},
-    {title: 'Nokia', price: 200, quantity: 19}
-]
+  { title: "Iphone", price: 800, quantity: 50 },
+  { title: "Samsung", price: 500, quantity: 39 },
+  { title: "Nokia", price: 200, quantity: 19 },
+];
 
-function total_summ (products) {
-    let summ = 0;
-    for (let i =0; i < products.length; i++){
-        summ += products[i].price;
-    }
-    alert(`Общая сумма товаров составляет ${summ} EUR`);
+function total_summ(products) {
+  let summ = 0;
+  for (let i = 0; i < products.length; i++) {
+    summ += products[i].price;
+  }
+  alert(`Общая сумма товаров составляет ${summ} EUR`);
 }
